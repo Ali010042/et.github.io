bdf62a635f78eec71f55dcb4ad33a674bc1c7be9
ali <112857919+Ali010042@users.noreply.github.com>
2024-04-30 20:30:53 +0200

    Add files via upload

---
+++ price.js
@@ -0,0 +1,114 @@
+// Product data arrays for each section
+const productsSection1 = [
+    { name: 'Frax Gezicht + Diepte Reiniging', price: '€ 100,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Frax Gezicht', price: '€ 80,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Frax Hals + Decolleté', price: '€ 145,00<p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Frax Lichaam( let op! prijs kan verschillen minimaal aanbetaling 50,- )', price: '<P>Vanaf</P>€ 50,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'TOSKANI PEELINGS', price: '<P>Vanaf</P>€ 125,29<p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'TCA Peeling', price: '<P>Vanaf</P>€ 150,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Basis Schoonheid', price: '<P>Vanaf</P>€ 65,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Uitgebreid Schoonheid', price: '€ 80,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Biopeeling', price: '€ 75,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Dermapen behandeling', price: '€ 125,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Wimper Lifting', price: '€ 55,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Vitamine Behandeling NCTF HA FILORGA', price: '€ 135,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+];
+
+const productsSection2 = [
+    { name: 'Consult', price: '€ 5,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+];
+
+const productsSection3 = [
+    { name: 'Epileren', price: '€ 10,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Epileren + wenkbrauwen verven', price: '€ 15,50 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Wimpers / Wenkbrauwen Verven', price: '€  15,50 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+];
+
+const productsSection4 = [
+    { name: 'Frax Gezicht + Diepte Reiniging', price: '€ 100,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Schouders', price: '€ 125,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Oksels', price: '€  40,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Hele Rug', price: '€ 200,29 <p>*Verplichte (aan)betaling</p> ', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN BovenRug OF OnderRug', price: '€ 125,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Borst + Buik', price: '€ 200,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Borst OF Buik', price: '€ 125,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN NEK', price: '€ 50,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Hals', price: '€ 50,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touc' },
+    { name: 'MANNEN Wangen', price: '€ 25,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Oren', price: '€ 25,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN Handen', price: '€ 25,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'MANNEN (samengestelde pakket) let op! alleen bestaande klanten', price: ' <p>*Vanaf</p>€ 25,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle laser ontharen vrouwen ( Hele lichaam )', price: '€ 400,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle 5 onderdelen', price: '€ 250,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Gezicht + Hals', price: '€ 75,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Gezicht', price: '€ 60,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bovenlip en kin', price: '€ 30,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bovenlip', price: '€ 20,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Kin', price: '€ 20,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bakkebaard', price: '€ 20,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Oksels', price: '€ 35,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Borsten', price: '€ 35,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Tepelhof', price: '€ 20,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Decolleté', price: '€ 30,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Buik', price: '€ 40,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Navelstreep', price: '€ 15,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Hele Rug', price: '€ 75,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bovenrug Of Onderrug', price: '€ 50,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Hele armen', price: '€ 100.29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bovenarm Of Onderarm', price: '€ 65.29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Handen', price: '€ 20,29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Hele benen', price: '€ 150,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bovenbenen', price: '€ 85,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Onderbenen', price: '€ 75,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Hele Billen met bilnaad', price: '€ 80,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Hele Billen zonder bilnaad', price: '€ 70,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bikini compleet', price: '€ 75,00 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+    { name: 'Gentle Bikinilijn', price: '€ 65.29 <p>*Verplichte (aan)betaling</p>', link: 'https://bjootify.com/salon/Rotterdam/esthetic-touch' },
+];
+
+function generateProductRowsSection1() {
+    const tbody = document.getElementById('price-table-body-section1');
+    generateProductRows(productsSection1, tbody);
+}
+
+
+function generateProductRowsSection2() {
+    const tbody = document.getElementById('price-table-body-section2');
+    generateProductRows(productsSection2, tbody);
+}
+
+
+function generateProductRowsSection3() {
+    const tbody = document.getElementById('price-table-body-section3');
+    generateProductRows(productsSection3, tbody);
+}
+
+
+function generateProductRowsSection4() {
+    const tbody = document.getElementById('price-table-body-section4');
+    generateProductRows(productsSection4, tbody);
+}
+
+
+function generateProductRows(products, tbody) {
+
+    tbody.innerHTML = '';
+
+    products.forEach(product => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${product.name}</td>
+            <td>${product.price}</td>
+            <td><a href="${product.link}" class="custom-button" target="_blank">Boek nu!</a></td>
+        `;
+        tbody.appendChild(row);
+    });
+}
+
+// Call the functions to generate product rows for each section when the page loads
+window.onload = function() {
+    generateProductRowsSection1();
+    generateProductRowsSection2();
+    generateProductRowsSection3();
+    generateProductRowsSection4();
+};
